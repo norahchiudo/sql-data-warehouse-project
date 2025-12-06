a5a0b214d7e7dfe4d901df0cb8d010ad1e02e6de
norahchiudo <norahchiudo1@gmail.com>
2025-12-06 10:32:00 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -123,7 +123,7 @@ BEGIN
 	SELECT 
 	REPLACE(cid, '-', '') AS cid,
 	CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
-		WHEN TRIM(cntry) IN ('US','USA') THEN 'United State'
+		WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
 		WHEN TRIM(cntry) ='' OR cntry IS NULL THEN 'n/a'
 		ELSE TRIM(cntry)
 		END AS cntry
